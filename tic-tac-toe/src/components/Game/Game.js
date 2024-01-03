ae6cd7cd328dc745d93ec519fb5c4b78e69be7ae
Luz Hernandez <hernandezluz18@gmail.com>
2024-01-03 13:54:47 -0500

    adding all updates

---
+++ tic-tac-toe/src/components/Game/Game.js
@@ -0,0 +1,59 @@
+import React , {useState} from "react";
+import Board from "../Board/Board";
+import { calculateWinner } from "../../helpers";
+
+const style = {
+    width: '200px',
+    margin: '20px auto'
+}
+
+const Game = () => {
+    const[history, setHistory] = useState([Array(9).fill(null)]);
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXisNext] = useState(true);
+    const winner = calculateWinner(history[stepNumber]);
+
+    const handleClick = i => {
+        const timeInHistory = history.slice(0, stepNumber + 1);
+        const current = timeInHistory[stepNumber];
+        const squares = [...current];
+        //if user clicks an occupied or if the game is won, return
+        if(winner || squares[i]) return;
+        //put an X or an O in the clicked square
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory([...timeInHistory, squares]);
+        setStepNumber(timeInHistory.length);
+        setXisNext(!xIsNext);
+    };
+
+    const jumpTo = step => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0);
+
+    };
+
+    const renderMoves = () => (
+        history.map((_step, move) => {
+            const destination = move ? `Go to move #${move}` : 'New Game';
+            return (
+                <li key={move}>
+                    <button onClick={() => jumpTo(move)}>{destination}</button>
+                </li> 
+            )
+        })
+
+    );
+
+    return (
+        <>
+            <h2>Tic Tac Toe</h2>
+            <Board squares={history[stepNumber]} onClick={handleClick} />
+            <div style={style}>
+                <p>{winner ? 'The Winner is: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+                {renderMoves()}
+            </div>
+        </>
+    )
+};
+
+export default Game;
