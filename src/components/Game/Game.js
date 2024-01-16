4fdc87bae6c0ebfa441fda4ba803eebe421545fd
Luz Hernandez <hernandezluz18@gmail.com>
2024-01-16 14:27:34 -0500

    updated logic of new game div

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -8,6 +8,7 @@ const Game = () => {
     const [stepNumber, setStepNumber] = useState(0);
     const [xIsNext, setXisNext] = useState(true);
     const winner = calculateWinner(history[stepNumber]);
+    const showWinner = false; 
 
     const handleClick = i => {
         const timeInHistory = history.slice(0, stepNumber + 1);
@@ -17,23 +18,37 @@ const Game = () => {
         if(winner || squares[i]) return;
         //put an X or an O in the clicked square
         squares[i] = xIsNext ? 'X' : 'O';
+        console.log(squares[i]);
         setHistory([...timeInHistory, squares]);
+        console.log("time in history: "+timeInHistory+ " squares: "+squares);
         setStepNumber(timeInHistory.length);
+        console.log("time in history: "+timeInHistory.length);
         setXisNext(!xIsNext);
     };
 
     const jumpTo = step => {
         setStepNumber(step);
         setXisNext(step % 2 === 0);
-        console.log(`Step `+step);
-        console.log(step%2);
+        console.log(`JumpTo func-->Step `+step);
+        console.log("Mod func-->"+step%2);
+    };
+
+    const shouldShowWinner = (winner) =>{
+        if(winner){
+            shouldShowWinner=true;
+        }
+        shouldShowWinner=false;
     };
 
     const renderMoves = () => (
         history.map((_step, move) => {
+            // const destination = move ? `Go back to move #${move}` : `New Game`;
             const destination = move ? `Go back to move #${move}` : `New Game`;
+            console.log("move "+ move +" "+ typeof(move));
+            console.log("step "+ _step +" "+ typeof(_step));
             return (
                 <div key={move} className="moves">
+                    {/* <button onClick={() => }>{newGame}</button> */}
                     <button className="movesButton" onClick={() => jumpTo(move)}>{destination}</button>
                 </div> 
             )
@@ -42,10 +57,11 @@ const Game = () => {
 
     return (
         <>
-            <h2 class="title">Tic Tac Toe</h2>
+            <h2 className="title">Tic Tac Toe</h2>
             <Board squares={history[stepNumber]} onClick={handleClick} />
             <div className="game">
-                <p class="result">{winner ? 'The Winner is: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
+                <p className="winner">{winner ? 'The Winner is: ' + winner : null}</p>
+                <p className="result">{'Next Player: ' + (xIsNext ? 'X' : 'O')}</p>
                 {renderMoves()}
             </div>
         </>
