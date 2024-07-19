7f8787e925abfa441714c8c9e7426b70b7a58892
Luz Hernandez <hernandezluz18@gmail.com>
2024-07-19 14:45:32 -0400

    removed unused code and improved naming convention of variables

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,14 +9,10 @@ const Game = () => {
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(history[stepNumber]);
 
-  const handleClick = (i) => {
+  const handleClick = () => {
     const timeInHistory = history.slice(0, stepNumber + 1);
     const current = timeInHistory[stepNumber];
     const squares = [...current];
-    //if user clicks an occupied space or if the game is won, return
-    if (winner || squares[i]) return;
-    //put an X or an O in the clicked square
-    squares[i] = xIsNext ? "X" : "O";
     setHistory([...timeInHistory, squares]);
     setStepNumber(timeInHistory.length);
     setXisNext(!xIsNext);
@@ -27,19 +23,19 @@ const Game = () => {
     setXisNext(step % 2 === 0);
   };
 
-  const renderMoves = () =>
+  const renderChangeMoveOptions = () =>
     history.map((_step, move) => {
-      const movesDisplay = `Go back to move #${move}`;
+      const moveChangeDisplay = `Go back to move #${move}`;
       return (
-        <div key={move} className="moves">
+        <>
           {move !== 0 ? (
-            <button className="moves-button" onClick={() => jumpTo(move)}>
-              {movesDisplay}
+            <button className="change-moves-button" onClick={jumpTo(move)}>
+              {moveChangeDisplay}
             </button>
           ) : (
             ``
           )}
-        </div>
+        </>
       );
     });
 
@@ -47,8 +43,8 @@ const Game = () => {
     <>
       <h2 className="title">Tic Tac Toe</h2>
       <p className="intro">
-        The game starts with your turn you are 'X'. You're able to go back and
-        change a move.
+        The game starts with 'X' turn. To go back and change a move click the
+        move you want to go back to.
       </p>
       <Board squares={history[stepNumber]} onClick={handleClick} />
       <div className="game">
@@ -70,7 +66,7 @@ const Game = () => {
             onClick={() => jumpTo(0)}
           >{`New Game`}</button>
         )}
-        {stepNumber === 0 ? `` : renderMoves()}
+        {stepNumber === 0 ? `` : renderChangeMoveOptions()}
       </div>
     </>
   );
